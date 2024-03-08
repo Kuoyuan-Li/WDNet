5a6f64264a8f13c97b3c4d66e5105c8ecdccf721
Kuoyuan-Li <kuoyuanli.reno@gmail.com>
2024-03-08 15:52:01 +1100

    test file edit

--- test.py
+++ test.py
@@ -14,41 +14,39 @@ G=generator(3,3)
 G.eval()
 G.load_state_dict(torch.load(os.path.join('WDNet_G.pkl')))
 G.cuda()
-root = './dataset/CLWD/test'
-imageJ_path=osp.join(root,'Watermarked_image','%s.jpg')
-img_save_path=osp.join('./results','result_img','%s.jpg')
-img_vision_path=osp.join('./results','result_vision','%s.jpg')
+image_path = './sample_images'
+img_save_path=osp.join('./results','result_img','%s')
+img_vision_path=osp.join('./results','result_vision','%s')
 ids = list()
-for file in os.listdir(root+'/Watermarked_image'):
-			#if(file[:-4]=='.jpg'):
-			ids.append(file.strip('.jpg'))
+for file in os.listdir(image_path):
+    if file.endswith('.jpg') or file.endswith('.png'):
+        ids.append(file)
+
 i=0
 all_time=0.0
 for img_id in ids:
-  i+=1
-  transform_norm=transforms.Compose([transforms.ToTensor()])
-  img_J=Image.open(imageJ_path%img_id)
-  img_source = transform_norm(img_J)
-  img_source=torch.unsqueeze(img_source.cuda(),0)
-  st=time.time()
-  pred_target,mask,alpha,w,I_watermark=G(img_source)
-  all_time+=time.time()-st
-  mean_time=all_time/i
-  print("mean time:%.3f"%mean_time)
-  p0=torch.squeeze(img_source)
-  p1=torch.squeeze(pred_target)
-  p2=mask
-  p3=torch.squeeze(w*mask)
-  p2=torch.squeeze(torch.cat([p2,p2,p2],1))
-  p0=torch.cat([p0,p1],1)
-  p2=torch.cat([p2,p3],1)
-  p0=torch.cat([p0,p2],2)
-  p0=transforms.ToPILImage()(p0.detach().cpu()).convert('RGB')
-  pred_target=transforms.ToPILImage()(p1.detach().cpu()).convert('RGB')
-  pred_target.save(img_save_path%img_id)
-  if i<=20:
-    p0.save(img_vision_path%img_id)
-  
-  
-  
-  
+    i+=1
+    transform_norm=transforms.Compose([transforms.ToTensor()])
+    img_J=Image.open(osp.join(image_path,img_id))
+    img_source = transform_norm(img_J)
+    img_source=torch.unsqueeze(img_source.cuda(),0)
+    st=time.time()
+    # call the generator to extract the target image and the mask
+    pred_target,mask,alpha,w,I_watermark=G(img_source)
+    all_time+=time.time()-st
+    mean_time=all_time/i
+    print("mean time:%.3f"%mean_time)
+
+    p0=torch.squeeze(img_source)
+    p1=torch.squeeze(pred_target)
+    p2=mask
+    p3=torch.squeeze(w*mask)
+    p2=torch.squeeze(torch.cat([p2,p2,p2],1))
+    p0=torch.cat([p0,p1],1)
+    p2=torch.cat([p2,p3],1)
+    p0=torch.cat([p0,p2],2)
+    p0=transforms.ToPILImage()(p0.detach().cpu()).convert('RGB')
+    pred_target=transforms.ToPILImage()(p1.detach().cpu()).convert('RGB')
+    pred_target.save(img_save_path%img_id)
+    if i<=20:
+        p0.save(img_vision_path%img_id)
